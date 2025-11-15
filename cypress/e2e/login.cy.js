18ea433e85db9eb0fa9bfe3851d78e08a1ce704d
Arthur Guimarães <arthurgruimaraes@gmail.com>
2025-11-15 16:34:23 -0300

    adicionando prints automaticos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,14 @@
 describe('Login - SauceDemo', () => {
   it('Deve logar com usuário válido', () => {
     cy.visit('https://www.saucedemo.com/')
+    cy.screenshot('01_pagina_login')
 
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
+    cy.screenshot('02_campos_preenchidos')
 
+    cy.get('[data-test="login-button"]').click()
     cy.url().should('include', '/inventory.html')
+    cy.screenshot('03_login_realizado')
   })
 })
